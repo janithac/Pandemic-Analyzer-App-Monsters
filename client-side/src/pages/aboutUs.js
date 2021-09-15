b2ccb14fe02083c63928d8e3825b9eccb535a43b
bhathiyaamarasinghe <bhathiyaamare98@gmail.com>
2021-09-16 00:45:50 +0530

    Revert "Revert "added routes""
    This reverts commit aeb609c755efd7aff4f607ce63af6467d72d4424.

---
+++ client-side/src/pages/aboutUs.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+
+export default function AboutUs() {
+  
+
+  return (
+   
+    <div><p>AboutUs</p></div>
+  );
+}
---
+++ client-side/src/pages/userForm.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+
+export default function UserForm() {
+  
+
+  return (
+   
+    <div><p>UserForm</p></div>
+  );
+}
