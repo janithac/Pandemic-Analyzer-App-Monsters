aeb609c755efd7aff4f607ce63af6467d72d4424
bhathiyaamarasinghe <bhathiyaamare98@gmail.com>
2021-09-15 22:59:26 +0530

    Revert "added routes"
    This reverts commit 9950c5cfe630ac1d02d01b0dbec25cb3135e699b.

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,23 +1,12 @@
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from './pages/home';
-// import AboutUs from './components/AboutUs';
-import Login from './pages/login';
-import SignUp from './pages/signup';
-// import AdminView from './components/AdminView';
+
+import './App.css';
 
 function App() {
   return (
-    <Router>
-    <Switch>
-      <Route path='/' exact component={Home}/>
-      {/* <Route path='/aboutus' component={AboutUs}/> */}
-      <Route path='/login' component={Login}/>
-      <Route path='/signup' component={SignUp}/>
-      {/* <Route path='/booknow' component={BookNow}/>
-      <Route path='/admin' component={AdminView}/> */}
-    </Switch>
-  </Router>
+    <div className="App">
+     <h1>Pandemic Analyzer</h1>
+    </div>
   );
 }
 
